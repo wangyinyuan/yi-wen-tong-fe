85a78e7f0ceaf9f1b978cb5e93d90c54d6a030bb
BigWang <2972437973@qq.com>
2024-01-03 02:35:38 +0800

    feat: 添加tabs和header的样式

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,9 @@
-import { Tabs } from "expo-router";
-import { lightTheme, darkTheme } from "@/constants/Color";
-import { useColorScheme } from "react-native";
 import type { ThemeType } from "@/constants/Color";
+import { darkTheme, lightTheme } from "@/constants/Color";
 import { LinearGradient } from "expo-linear-gradient";
+import { Tabs } from "expo-router";
+import { StyleSheet, useColorScheme } from "react-native";
+import { Entypo, FontAwesome5, Ionicons,MaterialCommunityIcons  } from "@expo/vector-icons";
 
 export default function TabsLayout() {
   // 导入颜色模式
@@ -13,11 +14,16 @@ export default function TabsLayout() {
     <Tabs
       initialRouteName="index"
       screenOptions={{
+        headerTitleAlign: "center",
+        headerTintColor: theme.headerTintColor,
+        headerStyle: {
+          backgroundColor: "#EB836E",
+        },
         tabBarShowLabel: false,
         tabBarActiveTintColor: theme.activatedTintColor,
         tabBarInactiveTintColor: theme.inactivatedTintColor,
         tabBarStyle: {
-          borderRadius: 15,
+          backgroundColor: "#EB836E",
         },
         tabBarBackground: () => {
           return (
@@ -25,14 +31,41 @@ export default function TabsLayout() {
               colors={[
                 theme.navigatorBackgroundStart,
                 theme.navigatorBackgroundEnd,
-              ]}></LinearGradient>
+              ]}
+              style={StyleSheet.absoluteFill}></LinearGradient>
           );
         },
       }}>
-      <Tabs.Screen name="index" />
-      <Tabs.Screen name="suggestion" />
-      <Tabs.Screen name="reminder" />
-      <Tabs.Screen name="report" />
+      <Tabs.Screen
+        name="index"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Entypo name="chat" size={size} color={color} />
+          ),
+          headerTitle: "AI医生在线提供咨询"
+        }}
+      />
+      <Tabs.Screen
+        name="suggestion"
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <FontAwesome5 name="heartbeat" size={size} color={color} />
+          ),
+          headerTitle: "个性化生活建议"
+        }}
+      />
+      <Tabs.Screen name="reminder"  options={{
+          tabBarIcon: ({ color, size }) => (
+            <Ionicons name="ios-time" size={size} color={color} />
+          ),
+          headerTitle: "用药提醒"
+        }} />
+      <Tabs.Screen name="report" options={{
+          tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="chart-box" size={size} color={color} />
+          ),
+          headerTitle: '个性健康报告'
+        }}/>
     </Tabs>
   );
 }
--- constants/Color.ts
+++ constants/Color.ts
@@ -3,6 +3,7 @@ export const lightTheme = {
   inactivatedTintColor: "rgba(255, 255, 255, 0.4)",
   navigatorBackgroundStart: "rgba(235, 92, 45, 0.58)",
   navigatorBackgroundEnd: "rgba(247, 79, 22, 0.73)",
+  headerTintColor: "#fff",
 };
 
 export const darkTheme = {};
